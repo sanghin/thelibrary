389f549149c05e5dac2a8a69268bfc4d406cda56
sanghin <damien.bonnay@protonmail.com>
2020-01-14 10:29:02 +0100

    :recycle: prettify example.test.js file

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,28 +1,27 @@
 import knexInstance from '../server/database/connection';
 
 jest.mock('knex', () => ({
-    clearHeaderData() {
+  clearHeaderData() {
 
-    },
-    clearLocale() {
-
-    }
+  },
+  clearLocale() {
+  },
 }));
 
 jest.mock('./db', () => ({
-    clearHeaderData() {
-
-    },
-    clearLocale() {
-
-    }
+  clearHeaderData() {
+  },
+  clearLocale() {
+  },
 }));
 
 
 describe('DB Connection', () => {
-    describe('Instantiate Connection', () => {
-        test('We have an instance if it exists already', () => {
-            knexInstance;
-        });
+  describe('Instantiate Connection', () => {
+    test('We have an instance if it exists already', () => {
+      // eslint-disable-next-line no-console
+      console.info(knexInstance);
+      expect(true).toBe(true);
     });
+  });
 });
